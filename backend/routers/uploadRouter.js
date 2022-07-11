09e7c6a1e359123de75a1525862ac443c711c3dd
kangsan <junbuck01@naver.com>
2022-07-11 14:37:04 +0900

    multer setup

--- backend/routers/uploadRouter.js
+++ backend/routers/uploadRouter.js
@@ -29,7 +29,7 @@ uploadRouter.post(
             return res.status(400).json({success: false, message: err.message});
 
         }
-        res.send('Uploaded : '+req.file.originalname);
+        res.send(req.file.originalname);
         console.log("multer req.file.orignal >>>>>>>  server res success >>>>>>>> "+ req.file.originalname);
     })
 )
