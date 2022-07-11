bb9255b32d4c66cfd81739bfaf140bc40e181810
kangsan <junbuck01@naver.com>
2022-07-11 21:17:15 +0900

    multer setup

--- backend/routers/uploadRouter.js
+++ backend/routers/uploadRouter.js
@@ -1,36 +1,52 @@
 const multer = require('multer');
+const multerS3 = require('multer-s3');
 const express = require('express');
 const expressAsyncHandler = require('express-async-handler');
 const { isAuth } = require('../utils.js');
+const AWS = require('aws-sdk');
 const uploadRouter = express.Router();
 
-var storage  = multer.diskStorage({ // 2
-    destination(req, file, cb) {
-        cb(null, 'uploads/');
-    },
-    filename(req, file, cb) {
-        cb(null, file.originalname);
-    },
+const s3 = new AWS.S3({
+    accessKeyId: process.env.S3_ACCESS_KEY,
+    secretAccessKey: process.env.S3_SECRET_KEY,
+    region: process.env.S3_BUCKET_REGION
 });
-const upload = multer({ storage: storage });
 
 
+const upload = (bucketName) =>
+    multer({
+        storageS3 : multerS3({
+            s3,
+            bucket: bucketName,
+            acl: 'public-read',
+            contentType: multerS3.AUTO_CONTENT_TYPE,
+            metadata: function (req, file, cb){
+                cb(null, {fieldName: file.fieldName});
+            },
+            key(req, file, cb){
+                cb(null, file.originalname); // 이름 설정
+            },
+    }),
+})
 
 
 uploadRouter.post(
     '/',
     isAuth,
-    upload.single('image'),
     expressAsyncHandler(async (req, res) => {
-        const image = req.file.originalname;
+        console.log("req.file >>>>>>>>>>>>>>> "+ JSON.stringify(req.file.destination));
 
-        console.log("multer req.file.orignal >>>>>>>>>>>>>>> "+ req.file.originalname);
-        if(image === undefined) {
-            return res.status(400).json({success: false, message: err.message});
+        const uploadSingle = upload("7znoe").single('image');
+        uploadSingle(req, res, (err) => {
+                if(err)
+                    return res.status(400).json({success: false, message: err.message});
+                console.log("req.file >>>>>>>  upload >>>>>>>> "+ req.file);
 
-        }
-        res.send(req.file.originalname);
-        console.log("multer req.file.orignal >>>>>>>  server res success >>>>>>>> "+ req.file.originalname);
+                res.status(200).json({data: req.file.destination})
+                console.log("req.file >>>>>>>  server res success >>>>>>>> "+ req.file.destination);
+
+            }
+        )
     })
 )
 
